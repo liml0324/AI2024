522d4a71a19ec302727ef97beb6fde68aeb0ed3e
lml <liml@mail.ustc.edu.cn>
2024-05-29 20:33:37 +0800

    Finished KMeans

--- labs/lab2/part_1/PCAKMeans.py
+++ labs/lab2/part_1/PCAKMeans.py
@@ -6,6 +6,10 @@ from gensim.models import KeyedVectors
 
 def get_kernel_function(kernel:str):
     # TODO: implement different kernel functions 
+    if kernel == "rbf":
+        def rbf(x1:np.ndarray, x2:np.ndarray):
+            return np.exp(-np.linalg.norm(x1 - x2) ** 2)
+        return rbf
     return None
 
 class PCA:
@@ -17,17 +21,34 @@ class PCA:
     def fit(self, X:np.ndarray):
         # X: [n_samples, n_features]
         # TODO: implement PCA algorithm
-        pass
+        m = X.shape[0]  # m是样本数
+        n = X.shape[1]  # n是特征数
+        # 进行中心化
+        X = X - np.mean(X, axis=0)
+        # 计算协方差矩阵
+        cov = np.dot(X.T, X) / (m - 1)  # 这里X是m*n维，cov是n*n维
+        # 计算特征值和特征向量
+        eig_vals, eig_vecs = np.linalg.eig(cov)
+        # 对特征值进行排序
+        eig_pairs = [(np.abs(eig_vals[i]), eig_vecs[:, i]) for i in range(n)]
+        eig_pairs.sort(key=lambda x: x[0], reverse=True)
+        # 选取前k个特征向量
+        k_eig_vecs = np.array([ele[1] for ele in eig_pairs[:self.n_components]])
+        # 得到投影矩阵
+        self.W = k_eig_vecs.T
+        return self
+        
 
     def transform(self, X:np.ndarray):
         # X: [n_samples, n_features]
-        X_reduced = np.zeros((X.shape[0], self.n_components))
+        # X_reduced = np.zeros((X.shape[0], self.n_components))
+        X_reduced = np.dot(X, self.W)
 
         # TODO: transform the data to low dimension
         return X_reduced
 
 class KMeans:
-    def __init__(self, n_clusters:int=3, max_iter:int=10) -> None:
+    def __init__(self, n_clusters:int=3, max_iter:int=1000) -> None:
         self.n_clusters = n_clusters
         self.max_iter = max_iter
         self.centers = None
@@ -54,6 +75,11 @@ class KMeans:
         self.labels = np.zeros(n_samples)
         # TODO: Compute the distance between each point and each center
         # and Assign each point to the closest center
+        for i in range(n_samples):
+            # Compute the distance between each point and each center
+            dists = np.linalg.norm(points[i] - self.centers, axis=1)
+            # Assign each point to the closest center
+            self.labels[i] = np.argmin(dists)
 
         return self.labels
 
@@ -61,13 +87,27 @@ class KMeans:
     def update_centers(self, points):
         # points: (n_samples, n_dims,)
         # TODO: Update the centers based on the new assignment of points
-        pass
+        for k in range(self.k):
+            if points[self.labels == k].shape[0] == 0:
+                continue
+            self.centers[k] = points[self.labels == k].mean(axis=0)
+        return self.centers
 
     # k-means clustering
     def fit(self, points):
         # points: (n_samples, n_dims,)
         # TODO: Implement k-means clustering
-        pass
+        self.k = self.n_clusters
+        self.initialize_centers(points)
+        for i in range(self.max_iter):
+            labels = self.labels
+            if labels is None:
+                labels = np.zeros(points.shape[0])
+            self.assign_points(points)
+            centers = self.update_centers(points)
+            if np.all(labels == self.labels):
+                break
+        return self
 
     # Predict the closest cluster each sample in X belongs to
     def predict(self, points):
@@ -90,6 +130,7 @@ def load_data():
     for w in words:
         vectors.append(w2v[w].reshape(1, 300))
     vectors = np.concatenate(vectors, axis=0)
+    print(len(words))
     return words, vectors
 
 if __name__=='__main__':
@@ -97,12 +138,12 @@ if __name__=='__main__':
     pca = PCA(n_components=2).fit(data)
     data_pca = pca.transform(data)
 
-    kmeans = KMeans(n_clusters=7).fit(data_pca)
+    kmeans = KMeans(n_clusters=7, max_iter=10).fit(data_pca)
     clusters = kmeans.predict(data_pca)
 
     # plot the data
     
-    plt.figure()
+    plt.figure(figsize=(10, 10))
     plt.scatter(data_pca[:, 0], data_pca[:, 1], c=clusters)
     for i in range(len(words)):
         plt.annotate(words[i], data_pca[i, :]) 
